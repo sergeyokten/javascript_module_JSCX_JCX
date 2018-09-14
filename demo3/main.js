01c0203d34fc461ac60068a96e7edf37e2ce16d7
sergeyokten <sergey.okten@gmail.com>
2018-09-14 13:59:05 +0300

    Initial commit

---
+++ demo3/main.js
@@ -0,0 +1,30 @@
+// let num = 11;
+//
+// if (num % 2 == 0) {
+//     console.log('even');
+// } else {
+//     console.log('odd');
+// }
+
+
+// let color = 'red';
+// if (color == 'green') {
+//     console.log('go!');
+// } else if (color == 'yellow') {
+//     console.log('wait');
+// } else if (color == 'red') {
+//     console.log('stop');
+// } else {
+//     console.log('default');
+// }
+
+
+let num = 68;
+
+if (num % 2 == 0 || num % 3 == 0) {
+    console.log('this number is multiple 2 & 3')
+}
+
+
+
+
